d4f9f31af0d8c311477d4fea54e2b05fce248ca4
IvanNenchev10 <56735762+IvanNenchev10@users.noreply.github.com>
2019-11-22 18:37:46 +0200

    Add files via upload

---
+++ Задача3.3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Задача3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b;
+            Console.WriteLine("Enter a:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            b = int.Parse(Console.ReadLine());
+            if(a>b)
+            {
+                Console.WriteLine("a is bigger than b");
+            }
+            else if(b>a)
+            {
+                Console.WriteLine("b is bigger than a");
+            }
+            else
+            {
+                Console.WriteLine("a is equal to b");
+            }
+        }
+    }
+}
